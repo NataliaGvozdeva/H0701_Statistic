b99934ccbc162b4758c4b2d15397ffdc58657d92
Natalia Gvozdeva (intern) <gvozdeva-natalia@mail.ru>
2023-02-17 23:40:06 +0300

    Deleted initialization and import artifact junit 4

--- src/test/java/ru/netology/stats/StatsServiceTest.java
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -1,7 +1,7 @@
 package ru.netology.stats;
 
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
 
 public class StatsServiceTest   {
 
@@ -15,7 +15,7 @@ public class StatsServiceTest   {
         long expected = 12;
         long actual = service.sumSales(statistic);
 
-        Assert.assertEquals(expected, actual);
+        Assertions.assertEquals(expected, actual);
     }
 
     //2.Среднюю сумму продаж в месяц
@@ -28,7 +28,7 @@ public class StatsServiceTest   {
         long expected = 1;
         long actual = service.middleSales(statistic);
 
-        Assert.assertEquals(expected, actual);
+        Assertions.assertEquals(expected, actual);
     }
 
     //3.Номер месяца, в котором был пик продаж,
@@ -42,7 +42,7 @@ public class StatsServiceTest   {
         long expected = 12;
         long actual = service.maxSales(statistic);
 
-        Assert.assertEquals(expected, actual);
+        Assertions.assertEquals(expected, actual);
     }
 
     //4.Номер месяца, в котором был минимум продаж,
@@ -56,7 +56,7 @@ public class StatsServiceTest   {
         long expected = 9;
         long actual = service.minSales(statistic);
 
-        Assert.assertEquals(expected, actual);
+        Assertions.assertEquals(expected, actual);
     }
 
     //5.Количество месяцев, в которых продажи были ниже среднего.
@@ -69,7 +69,7 @@ public class StatsServiceTest   {
         long expected = 3;
         long actual = service.amountMonthUnderMiddle(statistic);
 
-        Assert.assertEquals(expected, actual);
+        Assertions.assertEquals(expected, actual);
     }
 
     //6.Количество месяцев, в которых продажи были выше среднего.
@@ -82,6 +82,6 @@ public class StatsServiceTest   {
         long expected = 1;
         long actual = service.amountMonthUpperMiddle(statistic);
 
-        Assert.assertEquals(expected, actual);
+        Assertions.assertEquals(expected, actual);
     }
 }
